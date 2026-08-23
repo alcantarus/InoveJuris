dc6812750cce8b259f9947ba8c7d15949929c0a9
alcantarus <alcantarus@gmail.com>
2026-08-23 19:09:52 -0300

    chore(dev): remove turbopack flag and update lockfile
    Enable default Next.js build behavior by removing the --turbopack flag and synchronize dependencies in bun.lock.

--- dev.js
+++ dev.js
@@ -1,4 +1,4 @@
 const { spawn } = require('child_process');
-const child = spawn('npx', ['next', 'dev', '--turbopack=false', '-H', '0.0.0.0', '-p', '3000'], { stdio: 'inherit', shell: true });
+const child = spawn('npx', ['next', 'dev', '-H', '0.0.0.0', '-p', '3000'], { stdio: 'inherit', shell: true });
 child.on('exit', code => process.exit(code));
 
